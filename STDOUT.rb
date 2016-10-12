2bbb2588528e5a68a95565cbd14daf3dcfd01127
Nick Perez <nickperez1285@gmail.com>
2016-10-11 21:28:35 -0700

    added readme

--- STDOUT.rb
+++ STDOUT.rb
@@ -1,47 +1,47 @@
 class Primer
 # check if number is prime 
-def is_prime(num)
-	remainders = []
-	(2..num).to_a.each do |divisor|
+	def is_prime(num)
+		remainders = []
+		(2..num).to_a.each do |divisor|
 		# puts remainders of self divided by each number in range from 2 - itself
-		remainders << (num % divisor) 
-	end 
-	# a prime number should have only one remainder of 0  when divided by itself  
-	if remainders.count(0) > 1
-		return false 
-	else 
-		return true 
-	end 
-end 
-
-# create array of first n prime numbers 
-def prime_set(n)
-	primes_arr =[]
-	i = 1
-	# keep inserting prime numbers into array until its length is equal to 'n'
-	until primes_arr.count == n
-		if is_prime(i) == true 
-			primes_arr << i 
+			 << (num % divisor) 
 		end 
-		i+=1
+	# a prime number should have only one remainder of 0  when divided by itself  
+		if remainders.count(0) > 1
+			return false 
+		else 
+			return true 
+		end 
 	end 
-	p primes_arr
-end 
 
-# create multiplication table 
-def mult(arr)
-	# 1 space between every cell row added to row and column width
-	width = 1+ (arr.count * arr.count).to_s.length 
-	(arr).each do |x|
-		(arr).each do |y|
-			result =(x * y).to_s
-		#  for aesthtics 
-		padding = " " * (width - result.length)
-		print padding + result
+# creates array of first n prime numbers 
+	def prime_set(n)
+		primes_arr =[]
+		i = 1
+		# keep inserting prime numbers into array until its length is equal to 'n'
+		until primes_arr.count == n
+			if is_prime(i) == true 
+				primes_arr << i 
+			end 
+			i+=1
+		end 
+		p primes_arr
 	end 
-	print("\n")
-end
-end 
+
+# creates multiplication table using primes
+	def mult(arr)
+		# 1 space between every cell row added to row and column width
+		width = 1+ (arr.count * arr.count).to_s.length 
+		(arr).each do |x|
+			(arr).each do |y|
+				result =(x * y).to_s
+		#  for aesthtics 
+			padding = " " * (width - result.length)
+			print padding + result
+			end 
+		print("\n")
+		end
+	end 
 end 
 # prints out multiplication table for first 'n' prime numbers, currently set to 10
 # *******
